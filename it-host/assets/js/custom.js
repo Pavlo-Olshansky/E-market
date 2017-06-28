d7893cf79a1cc086143a4c79d50b0cb47a5bae07
Volodumur <tisvovanu4@gmail.com>
2017-06-28 21:56:50 +0300

    home_page_templates

---
+++ it-host/assets/js/custom.js
@@ -0,0 +1,50 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+       
+        reviews_fun:function()
+        {
+            ($)(function () {
+                $('#carousel-example').carousel({
+                    interval: 3000 //TIME IN MILLI SECONDS
+                });
+            });
+
+        },
+     
+        custom_fun:function()
+        {
+
+
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.reviews_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+

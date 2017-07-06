6808e8568b1f10a5778b68685d357027dcbcead4
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-07-06 14:55:17 +0300

    model post_save profile

--- accounts/models.py
+++ accounts/models.py
@@ -15,8 +15,8 @@ class UserProfile(models.Model):
     def __str__(self):
     	return self.user.username
 
-# @receiver(post_save, sender=User)
-def create_user_profile(sender, **kwargs):
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
     user_profile, created = UserProfile.objects.get_or_create(user=kwargs['instance'])
 
     # if kwargs['created']:
@@ -25,22 +25,24 @@ def create_user_profile(sender, **kwargs):
     # if created:
     #     user_profile = UserProfile.objects.create(user=instance)
 
-post_save.connect(create_user_profile, sender=User)
+# post_save.connect(create_user_profile, sender=User)
 
+
+@receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
 
-post_save.connect(save_user_profile, sender=User)
+# post_save.connect(save_user_profile, sender=User)
 
 
 # @receiver(post_save, sender=User)
 # def save_user_profile(sender, instance, **kwargs):
 #     instance.profile.save()
 
-# @receiver(post_save, sender=User)
+@receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.get_or_create(user=instance)
     instance.userprofile.save()
-post_save.connect(update_user_profile, sender=User)
+# post_save.connect(update_user_profile, sender=User)
 

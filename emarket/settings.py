a7df2c7f8b0a75db36a80c8a342ca3957037213a
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-07-06 18:07:25 +0300

    configured to deploy to heroku

--- emarket/settings.py
+++ emarket/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'emarket.urls'

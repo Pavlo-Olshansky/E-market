9d6df68c5618e01aa2ffea72a8c384804ad8abd0
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-07-06 15:03:07 +0300

    model post_save profile

--- accounts/models.py
+++ accounts/models.py
@@ -19,7 +19,7 @@ class UserProfile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    user_profile, created = UserProfile.objects.get_or_create(user=kwargs['instance'])
+    user_profile, created = UserProfile.objects.get_or_create(user=instance)
 
     # if kwargs['created']:
     #     user_profile.set_password(user_profile.password)

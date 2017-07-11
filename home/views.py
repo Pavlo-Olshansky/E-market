3293c893217f87bb147c589496f046e753c86aaa
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-07-11 03:57:45 +0300

    added get_client_ip function

--- home/views.py
+++ home/views.py
@@ -4,3 +4,12 @@ from django.views.generic.base import TemplateView
 class HomePage(TemplateView):
 
 	template_name = 'home.html'
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
